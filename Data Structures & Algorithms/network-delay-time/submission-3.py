4cd1ef8e76fc75cd67dde27be8c4a11a4fe86444
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-06-18 11:23:10 -0700

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # build the graph: O(E) loop through all edges
+        adj_list = defaultdict(dict)
+        for u, v, w in times:
+            adj_list[u][v] = w
+
+        # Dijkstra: O(ElogV), directed graph E is bounded by V^2
+        min_heap = [(0, k)]     # (cost, node)
+        visited = set()
+        total_cost = 0
+
+        # loop until all nodes are visited or there is no edge left to explore
+        while min_heap and len(visited) < n:
+            cost, curr = heapq.heappop(min_heap)
+
+            if curr in visited:
+                continue
+            visited.add(curr)
+
+            # mark total cost and keep exploring from curr node
+            total_cost = cost
+            for nxt in adj_list[curr]:
+                if nxt not in visited:
+                    nxt_cost = cost + adj_list[curr][nxt]
+                    heapq.heappush(min_heap, (nxt_cost, nxt))
+        
+        return total_cost if len(visited) == n else -1
