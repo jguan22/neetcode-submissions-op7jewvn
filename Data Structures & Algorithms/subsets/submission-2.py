c5963c7a3fd780d98bf224a7362d56a5a397f50e
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-07-18 21:24:30 -0700

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # backtracking 2^n states: O(n * 2^n)
+        n = len(nums)
+        subsets = []
+        curr_set = []
+
+        def backtrack(i):
+            if i == n:
+                subsets.append(curr_set[:])
+                return
+            
+            # either add curr num or skip it
+            curr_set.append(nums[i])
+            backtrack(i+1)
+
+            curr_set.pop()
+            backtrack(i+1)
+        
+        backtrack(0)
+        return subsets
+                
