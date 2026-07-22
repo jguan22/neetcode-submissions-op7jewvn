c17ea15867513470d8651c7f281bf013946db5a9
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-07-22 11:21:41 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp problem: O(n)
+        n = len(nums)
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+        return dp[n]
