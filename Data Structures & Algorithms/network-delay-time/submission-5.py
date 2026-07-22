41209369ddae16b9afc6b94755fbfd7d9ede2f83
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-07-21 21:58:09 -0700

    Add: network-delay-time - submission-5

---
+++ Data Structures & Algorithms/network-delay-time/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # build the adjacent list for the graph: O(E)
+        adj_list = defaultdict(dict)
+        for u, v, w in times:
+            adj_list[u][v] = w
+        
+        # use Dijkstra, starting from node k: O(ElogV)
+        min_heap = [(0, k)]
+        visited = set()
+
+        while min_heap:
+            cost, curr = heapq.heappop(min_heap)
+
+            if curr in visited:
+                continue
+            visited.add(curr)
+
+            if len(visited) == n:
+                return cost
+
+            for nxt in adj_list[curr]:
+                if nxt not in visited:
+                    heapq.heappush(min_heap, (cost + adj_list[curr][nxt], nxt))
+        
+        return -1
+
+        
