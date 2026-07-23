89cb55d7f25b4dac1398f832dcd3d7ed3e2965d3
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-07-23 12:48:11 -0700

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # sanity check: gas must be larger than cost
+        if sum(gas) < sum(cost):
+            return -1
+        
+        # if not, there is guaranteed to be one valid answer
+        # linear scan to track on the remaining gas vs nxt cost: O(n)
+        # start over when not enough gas to go on
+        start_index = 0
+        remaining_gas = 0
+        n = len(gas)
+        for i in range(n):
+            remaining_gas += gas[i] - cost[i]
+            if remaining_gas < 0:
+                start_index = i + 1
+                remaining_gas = 0
+        
+        return start_index
