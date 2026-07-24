1dd51b288d60572f1170bb1722ecbf684a82b4d3
Jiehao Guan <112458901+jguan22@users.noreply.github.com>
2026-07-24 12:40:24 -0700

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # dp problem with memo
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[1] * n for _ in range(m)]
+
+        directions = [
+            (-1, 0), 
+            (1, 0),
+            (0, -1),
+            (0, 1)
+        ]
+
+        # memo for dfs run to store prev result
+        memo = {}
+
+        def dfs(x, y):
+            # base case: explore from this cell already
+            if (x, y) in memo:
+                return memo[(x, y)]
+            
+            curr_max = 1
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and matrix[nx][ny] > matrix[x][y]:
+                    curr_max = max(curr_max, dfs(nx, ny) + 1)
+            
+            memo[(x, y)] = curr_max
+            return curr_max
+        
+        # loop through the matrix
+        max_path = 0
+        for i in range(m):
+            for j in range(n):
+                curr_path = dfs(i, j)
+                max_path = max(max_path, curr_path)
+        
+        return max_path
